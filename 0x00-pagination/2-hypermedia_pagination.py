7fcf510bde64d5bb2ea17ca99c49b0031bb75307
badrxd <badrxdxd@gmail.com>
2024-01-28 00:49:22 +0100

    2. Hypermedia pagination

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -61,14 +61,3 @@ class Server:
             'prev_page': prev_pg,
             'total_pages': math.ceil(ln/page_size)
         }
-
-
-server = Server()
-
-print(server.get_hyper(1, 2))
-print("---")
-print(server.get_hyper(2, 2))
-print("---")
-print(server.get_hyper(100, 3))
-print("---")
-print(server.get_hyper(3000, 100))
