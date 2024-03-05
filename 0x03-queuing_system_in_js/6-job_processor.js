4829c498f4cc5f6cbfbaa3b712c44accb983b6fc
badrxd <badrxdxd@gmail.com>
2024-03-05 06:12:17 +0100

    task 7. Create the Job processor

---
+++ 0x03-queuing_system_in_js/6-job_processor.js
@@ -0,0 +1,18 @@
+import { createQueue } from "kue";
+
+const queue = createQueue();
+
+const sendNotification = (phoneNumber, message) => {
+  console.log(
+    `Sending notification to ${phoneNumber}, with message: ${message}`
+  );
+};
+
+// queue.on("push_notification_code", function (notification) {
+//   sendNotification(...notification);
+// });
+
+queue.process("push_notification_code", function (job, done) {
+  sendNotification(job.data.phoneNumber, job.data.message);
+  done();
+});
